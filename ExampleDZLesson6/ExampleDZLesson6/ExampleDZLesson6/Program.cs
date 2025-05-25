e1e0100c05edabafb54f5ebda96f47eecb2d92b0
KAMO-PC\Admin <you@example.com>
2025-05-25 22:28:38 +0300

    DZ6

---
+++ ExampleDZLesson6/ExampleDZLesson6/ExampleDZLesson6/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace ExampleDZLesson6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager
+            {
+                Name = "Алексей ",
+                Position = "Мененджер",
+                WorkExperience = 365,
+                CurrentProject = new Manager.Project
+                {
+                    ProjectName =  "Разработка приложений",
+                    DeadLine = new DateTime(2025,12,31)
+                } 
+            };
+
+            Manager TwoManager = new Manager
+            {
+                Name = "Володя ",
+                Position = "Мененджер",
+                WorkExperience = 257
+            };
+
+            Worker worker = new Worker
+            {
+                Name = "Виктория",
+                Position = "Дизайнер",
+                Speciality = "Графика",
+
+            };
+
+            Worker TwoWorker = new Worker()
+            {
+                Name = "Алексей",
+                Position = "Программист",
+                Speciality = "Python",
+            };
+
+            EmployeeRegistry.AddEmployee(worker);
+
+            EmployeeRegistry.AddEmployee(manager);
+
+            EmployeeRegistry.AddEmployee(TwoManager);
+
+            EmployeeRegistry.AddEmployee(TwoWorker);
+
+            EmployeeRegistry.ListAllEmployees();
+
+            var findEmploy = EmployeeRegistry.FindEmployee("Виктория");
+
+            if (findEmploy != null)
+            {
+                findEmploy.GetDetails();
+            }
+            else
+            {
+                Console.WriteLine("Не найдено!");
+            }
+
+        }
+    }
+}
---
+++ ExampleDZLesson6/ExampleDZLesson6/ExampleDZLesson6/Worker.cs
@@ -0,0 +1,10 @@
+﻿namespace ExampleDZLesson6;
+internal class Worker : EmployeeBase
+{
+    public string Speciality { get; set; }
+    public override void GetDetails()
+    {
+        Console.WriteLine($"Имя {Name}, должность {Position}, Специальность {Speciality}");
+    }
+}
+
