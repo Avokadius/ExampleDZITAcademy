b7e8116e77ae69ae786c977f452a552a1ce82d1a
Denis Dudin <138156258+Avokadius@users.noreply.github.com>
2025-05-25 22:36:42 +0300

    Delete ExampleDZLesson6/ExampleDZLesson6 directory

--- ExampleDZLesson6/ExampleDZLesson6/ExampleDZLesson6/EmployeeBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExampleDZLesson6;
-public abstract class EmployeeBase
-{
-    public string Name { get; set; }
-    public string Position { get; set; }
-
-    //Метод абстрактный
-    public abstract void GetDetails();
-}
